e81abf3fff45e9ec99945e8d327505edbc2a8e6a
shamkumbhar <shamkumbhar47@gmail.com>
2025-05-27 17:09:10 +0530

    Update dockerhub_cred.groovy

--- vars/dockerhub_cred.groovy
+++ vars/dockerhub_cred.groovy
@@ -1,13 +1,6 @@
-def call(String credId, String imageName){
-  withCredentials([usernamePassword(
-                    credentialsId:"${credId}",
-                    passwordVariable: "dockerHubPass",
-                    usernameVariable: "dockerHubUser"
-                )]){
-                
-                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                sh "docker image tag ${imageName} ${env.dockerHubUser}/${imageName}"
-                sh "docker push ${env.dockerHubUser}/${imageName}:latest"
-            
-                }  
+def call(String Project, String ImageTag, String dockerhubuser , string credentialsId ){
+  withCredentials([usernamePassword(credentialsId: '${credentialsId}', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  }
+  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
